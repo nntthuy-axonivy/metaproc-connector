7e185428d9cf3610614adc505e7712e9f507cf44
Tu Thanh Nguyen <138571181+tutn-axonivy@users.noreply.github.com>
2024-05-27 14:57:27 +0700

    MARP-228-Problem-with-meta-proc-connector (#15)

--- metaproc-connector/src/com/axonivy/connector/metaproc/auth/AuthTokenFeature.java
+++ metaproc-connector/src/com/axonivy/connector/metaproc/auth/AuthTokenFeature.java
@@ -1,6 +1,7 @@
 package com.axonivy.connector.metaproc.auth;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
@@ -53,11 +54,14 @@ public class AuthTokenFeature implements Feature {
               .accept(MediaType.APPLICATION_JSON).post(entity)) {
         var node = response.readEntity(ObjectNode.class);
         TOKEN = node.get("loginResult").get("token").asText();
-        TOKEN_EXPIRED_TIME = System.currentTimeMillis() + Long.valueOf(Ivy.var().get("metaproc.TokenExpiredDuration"));
+        TOKEN_EXPIRED_TIME = System.currentTimeMillis() + getExpiredDuration();
         context.getHeaders().add(TOKEN_HEADER, TOKEN);
-      } catch (Exception e) {
-        Ivy.log().error(e.getMessage());
       }
     }
+  }
+  
+  private long getExpiredDuration() {
+    var seconds = Long.valueOf(Ivy.var().get("metaproc.TokenExpiredDuration"));
+    return TimeUnit.SECONDS.toMillis(seconds);
   }
 }
